79ec711362965c849eddbd619d5ce94341f12f79
Megan Frisella <meganfrisella@gmail.com>
2022-12-08 20:13:00 -0500

    stated property I'd like to prove in lean

--- src/shell_commands.lean
+++ src/shell_commands.lean
@@ -32,6 +32,10 @@ structure stream_of_strings :=
   (per_line : list line_pred)
   (bet_line : list doc_pred)
 
+/-----------------------------------------
+|                  SORT                  |
+-----------------------------------------/
+
 -- four ordering predicates, one for each sort variant
 -- along with corresponding ordering functions
 constants has_def_order has_r_order has_n_order has_rn_order : doc_pred
@@ -40,31 +44,25 @@ axiom make_sort_r : list record → list record
 axiom make_sort_n : list record → list record
 axiom make_sort_rn : list record → list record
 
--- a predicate that says "no adjacent lines are the same"
--- along with corresponding uniq functions
-constants adj_lines_uniq : doc_pred
-axiom make_uniq : list record → list record
-axiom make_uniq_c : list record → list record
-
-/----------------  sort  ----------------/
+-- sort
 -- sort alphabetically in ascending order
 -- no requirements on input
-noncomputable def sort (input : stream_of_strings) : option stream_of_strings := 
-  some { records := make_sort input.records,
+noncomputable def sort (input : stream_of_strings) : stream_of_strings := 
+  { records := make_sort input.records,
     column_types := input.column_types,
     per_line := input.per_line,
     bet_line := [has_def_order] }
 
-/---------------  sort -r  ---------------/
+-- sort -r
 -- sort alphabetically in descending order
 -- no requirements on input
-noncomputable def sort_r (input : stream_of_strings) : option stream_of_strings := 
-  some { records := make_sort_r input.records,
+noncomputable def sort_r (input : stream_of_strings) : stream_of_strings := 
+  { records := make_sort_r input.records,
     column_types := input.column_types,
     per_line := input.per_line,
     bet_line := [has_r_order] }
 
-/---------------  sort -n  ---------------/
+-- sort -n
 -- sort numerically in ascending order
 -- helper checks that the first column has numeric type
 noncomputable def sort_n_helper : stream_of_strings → list type → option stream_of_strings 
@@ -76,7 +74,7 @@ noncomputable def sort_n_helper : stream_of_strings → list type → option str
 noncomputable def sort_n (input : stream_of_strings) : option stream_of_strings := 
   sort_n_helper input input.column_types
 
-/---------------  sort -rn  ---------------/
+-- sort -rn
 -- sort numerically in descending order
 -- helper checks that the first column has numeric type
 noncomputable def sort_rn_helper : stream_of_strings → list type → option stream_of_strings 
@@ -88,21 +86,38 @@ noncomputable def sort_rn_helper : stream_of_strings → list type → option st
 noncomputable def sort_rn (input : stream_of_strings) : option stream_of_strings := 
   sort_n_helper input input.column_types
 
-/---------------  uniq  ---------------/
+/-----------------------------------------
+|                  UNIQ                  |
+-----------------------------------------/
+
+-- a predicate that says "no adjacent lines are the same"
+-- along with corresponding uniq functions
+constants adj_lines_uniq : doc_pred
+axiom make_uniq : list record → list record
+axiom make_uniq_c : list record → list record
+
+-- uniq
 -- filter out adjacent repeated lines
-noncomputable def uniq (input : stream_of_strings) : option stream_of_strings := 
-  some { records := make_uniq input.records,
+noncomputable def uniq (input : stream_of_strings) : stream_of_strings := 
+  { records := make_uniq input.records,
     column_types := input.column_types,
     per_line := input.per_line,
     bet_line := adj_lines_uniq :: input.bet_line }
-
-/---------------  uniq -c  ---------------/
+-- uniq -c
 -- filter out adjacent repeated lines and prefix the lines with 
 -- a count column
-noncomputable def uniq_c (input : stream_of_strings) : option stream_of_strings := 
-  some { records := make_uniq_c input.records,
+noncomputable def uniq_c (input : stream_of_strings) : stream_of_strings := 
+  { records := make_uniq_c input.records,
     column_types := type.num :: input.column_types,
     per_line := input.per_line,
     bet_line := adj_lines_uniq :: input.bet_line }
+
+/-----------------------------------------
+|              VERIFICATION              |
+-----------------------------------------/
+
+def no_duplicate_lines : stream_of_strings → Prop := sorry
+theorem sort_then_uniq (input : stream_of_strings) : no_duplicate_lines (sort (uniq input))
+  := sorry
 
 end shell
