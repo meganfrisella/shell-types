dac15fb23bf771eba6cc80c7e2b4a52fcc8b4342
Megan Frisella <meganfrisella@gmail.com>
2022-12-09 17:54:32 -0500

    make computable

--- src/shell_commands.lean
+++ src/shell_commands.lean
@@ -37,17 +37,21 @@ structure stream_of_strings :=
 -----------------------------------------/
 
 -- four ordering predicates, one for each sort variant
--- along with corresponding ordering functions
-constants has_def_order has_r_order has_n_order has_rn_order : doc_pred
-axiom make_sort : list record → list record
-axiom make_sort_r : list record → list record
-axiom make_sort_n : list record → list record
-axiom make_sort_rn : list record → list record
+def has_def_order (input : list record) : Prop := sorry
+def has_r_order (input : list record) : Prop := sorry
+def has_n_order (input : list record) : Prop := sorry
+def has_rn_order (input : list record) : Prop := sorry
+
+-- implementations of the four ordering functions
+def make_sort (input : list record) : list record := sorry
+def make_sort_r (input : list record) : list record := sorry
+def make_sort_n (input : list record) : list record := sorry
+def make_sort_rn (input : list record) : list record := sorry
 
 -- sort
 -- sort alphabetically in ascending order
 -- no requirements on input
-noncomputable def sort (input : stream_of_strings) : stream_of_strings := 
+def sort (input : stream_of_strings) : stream_of_strings := 
   { records := make_sort input.records,
     column_types := input.column_types,
     per_line := input.per_line,
@@ -56,7 +60,7 @@ noncomputable def sort (input : stream_of_strings) : stream_of_strings :=
 -- sort -r
 -- sort alphabetically in descending order
 -- no requirements on input
-noncomputable def sort_r (input : stream_of_strings) : stream_of_strings := 
+def sort_r (input : stream_of_strings) : stream_of_strings := 
   { records := make_sort_r input.records,
     column_types := input.column_types,
     per_line := input.per_line,
@@ -65,25 +69,25 @@ noncomputable def sort_r (input : stream_of_strings) : stream_of_strings :=
 -- sort -n
 -- sort numerically in ascending order
 -- helper checks that the first column has numeric type
-noncomputable def sort_n_helper : stream_of_strings → list type → option stream_of_strings 
+def sort_n_helper : stream_of_strings → list type → option stream_of_strings 
 | sos (type.num :: ts) := some {records := make_sort_n sos.records,
                                 column_types := sos.column_types,
                                 per_line := sos.per_line,
                                 bet_line := [has_n_order]}
 | _ _ := none
-noncomputable def sort_n (input : stream_of_strings) : option stream_of_strings := 
+def sort_n (input : stream_of_strings) : option stream_of_strings := 
   sort_n_helper input input.column_types
 
 -- sort -rn
 -- sort numerically in descending order
 -- helper checks that the first column has numeric type
-noncomputable def sort_rn_helper : stream_of_strings → list type → option stream_of_strings 
+def sort_rn_helper : stream_of_strings → list type → option stream_of_strings 
 | sos (type.num :: ts) := some {records := make_sort_rn sos.records,
                                 column_types := sos.column_types,
                                 per_line := sos.per_line,
                                 bet_line := [has_rn_order]}
 | _ _ := none
-noncomputable def sort_rn (input : stream_of_strings) : option stream_of_strings := 
+def sort_rn (input : stream_of_strings) : option stream_of_strings := 
   sort_n_helper input input.column_types
 
 /-----------------------------------------
@@ -91,22 +95,24 @@ noncomputable def sort_rn (input : stream_of_strings) : option stream_of_strings
 -----------------------------------------/
 
 -- a predicate that says "no adjacent lines are the same"
--- along with corresponding uniq functions
-constants adj_lines_uniq : doc_pred
-axiom make_uniq : list record → list record
-axiom make_uniq_c : list record → list record
+def adj_lines_uniq (input : list record) : Prop := sorry
+
+-- implementations of the two uniq functions
+def make_uniq (input : list record) : list record := sorry
+def make_uniq_c (input : list record) : list record := sorry
 
 -- uniq
 -- filter out adjacent repeated lines
-noncomputable def uniq (input : stream_of_strings) : stream_of_strings := 
+def uniq (input : stream_of_strings) : stream_of_strings := 
   { records := make_uniq input.records,
     column_types := input.column_types,
     per_line := input.per_line,
     bet_line := adj_lines_uniq :: input.bet_line }
+
 -- uniq -c
 -- filter out adjacent repeated lines and prefix the lines with 
 -- a count column
-noncomputable def uniq_c (input : stream_of_strings) : stream_of_strings := 
+def uniq_c (input : stream_of_strings) : stream_of_strings := 
   { records := make_uniq_c input.records,
     column_types := type.num :: input.column_types,
     per_line := input.per_line,
@@ -116,8 +122,15 @@ noncomputable def uniq_c (input : stream_of_strings) : stream_of_strings :=
 |              VERIFICATION              |
 -----------------------------------------/
 
-def no_duplicate_lines : stream_of_strings → Prop := sorry
-theorem sort_then_uniq (input : stream_of_strings) : no_duplicate_lines (sort (uniq input))
+inductive is_in {α : Type} : α → list α → Prop
+| intro_head (a : α) (as : list α) : is_in a (a::as)
+| intro_rest (a b : α) (as : list α) : is_in a as → is_in a (b::as)
+
+inductive no_duplicates {α : Type} : list α → Prop
+| intro_base : no_duplicates list.nil
+| intro_rest (a : α) (as : list α) : ¬(is_in a as) → no_duplicates as → no_duplicates (a::as)
+
+theorem sort_then_uniq (input : stream_of_strings) : no_duplicates (sort (uniq input)).records 
   := sorry
 
 end shell
